17b8f9a069ef896865417454ee25a5b12e59e6ff
Mayank Raj <mayank42293@gmail.com>
2022-05-29 14:54:58 +0530

    29/5/22

---
+++ String Problems/Minimal_Rotation.cpp
@@ -0,0 +1,233 @@
+//                           ,;;;;;;,
+//                                 ,;;;'""`;;\
+//                               ,;;;/  .'`',;\
+//                             ,;;;;/   |    \|_
+//                            /;;;;;    \    / .\
+//                          ,;;;;;;|     '.  \/_/
+//                         /;;;;;;;|       \
+//              _,.---._  /;;;;;;;;|        ;   _.---.,_
+//            .;;/      `.;;;;;;;;;|         ;'      \;;,
+//          .;;;/         `;;;;;;;;;.._    .'         \;;;.
+//         /;;;;|          _;-"`       `"-;_          |;;;;\
+//        |;;;;;|.---.   .'  __.-"```"-.__  '.   .---.|;;;;;|
+//        |;;;;;|     `\/  .'/__\     /__\'.  \/`     |;;;;;|
+//        |;;;;;|       |_/ //  \   //  \ \_|       |;;;;;|
+//        |;;;;;|       |/ |/    || ||    \| \|       |;;;;;|
+//         \;;;;|    __ || _  .-.\| |/.-.  _ || __    |;;;;/
+//          \jgs|   / _\|/ = /_o_\   /_o_\ = \|/_ \   |;;;/
+//           \;;/   |`.-     `   `   `   `     -.`|   \;;/
+//          _|;'    \ |    _     _   _     _    | /    ';|_
+//         / .\      \_  ( '--'(     )'--' )  _//      /. \
+//         \/_/       \_/|  /_   |   |   _\  |\_/       \_\/
+//                       | /|\  \   /  //|\ |
+//                       |  | \'._'-'_.'/ |  |
+//                       |  ;  '-.```.-'  ;  |
+//                       |   \    ```    /   |
+//     __                ;    '.-"""""-.'    ;                __
+//    /\ \_         __..--\     `-----'     /--..__         _/ /\
+//    \_'/\`''---''`..;;;;.'.__,       ,__.',;;;;..`''---''`/\'_/
+//         '-.__'';;;;;;;;;;;,,'._   _.',,;;;;;;;;;;;''__.-'
+//              ``''--; ;;;;;;;;..`"`..;;;;;;;; ;--''``   _
+//         .-.       /,;;;;;;;';;;;;;;;;';;;;;;;,\    _.-' `\
+//       .'  /_     /,;;;;;;'/| ;;;;;;; |\';;;;;;,\  `\     '-'|
+//      /      )   /,;;;;;',' | ;;;;;;; | ',';;;;;,\   \   .'-./
+//      `'-..-'   /,;;;;','   | ;;;;;;; |   ',';;;;,\   `"`
+//               | ;;;','     | ;;;;;;; |  ,  ', ;;;'|
+//              _\__.-'  .-.  ; ;;;;;;; ;  |'-. '-.__/_
+//             / .\     (   )  \';;;;;'/   |   |    /. \
+//             \/_/   (`     `) \';;;'/    '-._|    \_\/
+//                     '-/ \-'   '._.'         `
+//                       """      /.`\
+//                                \|_/
+
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mk make_pair
+#define ll long long
+#define ss second
+#define ff first
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define mll map<ll,ll>
+//#define mod 1000000007
+#define sp " "
+#define w(x) ll x; cin>>x; while(x--)
+#define ps(x,y) fixed<<setprecision(y)<<x;
+#define fo(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define re(i, j) fo(i, 0, j, 1)
+#define pi 3.1415926535897932384626433832795
+#define all(cont) cont.begin(), cont.end()
+#define countbit(x) __builtin_popcount(x)
+#define mod 1000000007//998244353
+#define lo lower_bound
+#define de(n) ll n;cin>>n;
+#define def(a,n) ll n;cin>>n;ll a[n];re(i,n){cin>>a[i];}
+#define defi(a,n,k) ll n;cin>>n; ll k;cin>>k;ll a[n];re(i,n){cin>>a[i];}
+#define deb(x) cout<<#x<<"="<<x<<endl;
+#define tr(it,a) for(auto it=a.begin();it!=a.end();it++)
+#define nl cout<<endl;
+#define minato ios_base::sync_with_stdio(false), cin.tie(nullptr)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define precision(x) cout << fixed << setprecision(x);
+#define gcd(a,b)    __gcd((a),(b))
+#define lcm(a,b)    ((a)*(b)) / gcd((a),(b))
+//#define endl "\n"
+#define int long long
+const int dx[4]={1,0,0,-1}, dy[4]={0,1,-1,0};
+const int x_dir[]={-1,-1,-1,0,0,1,1,1};
+const int y_dir[]={-1,0,1,-1,1,-1,0,1};
+
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef KNIGHTMARE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+//void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//KnightMareVoid
+const int MAX=2e6+10;
+ll fac[MAX];
+ll infact[MAX];
+int modu(ll x){
+while(x<0)
+  x+=mod;
+return (x%mod);
+}
+ll power(ll x,ll y){
+    ll p=mod;
+    ll res =1;
+    x=x%p;
+    while(y>0){
+        if(y&1){
+            res=(res*x)%p;
+        }
+            y=y>>1;
+            x=(x*x)%p;
+        }
+        return res;
+
+    
+}
+ll modInverse(ll n){
+    ll p=mod;
+    return power(n,p-2);
+}
+vector<int> poww;
+vector<int> h;
+vector<int> p1;
+void hashed(string s){
+    int n=s.size();
+    poww.assign(n,0);
+    int m=mod;
+    int p=31;
+
+    poww[0]=1;
+    for(int i=1;i<n;i++){
+        poww[i]=(poww[i-1]*p)%m;
+    }
+    h.assign(n+1,0);
+    for(int i=0;i<n;i++){
+        h[i+1]=(h[i]+((s[i]-'a'+1)*poww[i])%m)%m;
+
+    }
+    p1.assign(n,0);
+    int x=1;
+    for(int i=0;i<n;i++){
+        p1[i]=(power(x,mod-2))%mod;
+        x*=p;
+        x%=mod;
+    }
+    return;
+
+
+}
+int get(int l,int r){
+
+    
+    //cout<<l<<sp<<r<<endl;
+    int x=((h[r+1]-h[l]+mod)%mod)*p1[l]%mod;
+    //cout<<x<<endl;
+    return x;
+
+    
+}
+int solve(){
+    string s;
+    cin>>s;
+    s+=s;
+    hashed(s);
+    int k = 1;
+int n = s.size() / 2;
+for(int i = 2; i <= n; i++) {
+  int lo = 0, hi = n ;
+  while(lo+1<hi){//find length of common prefix using binary search
+      int mid=(lo+hi)/2;
+    //   if(i==8){
+
+    //       cout<<s.substr(i,mid+1)<<sp<<s.substr(k,mid+1)<<endl;
+    //       cout<<get(i,i+mid)<<sp<<get(k,k+mid)<<endl;
+          
+    //   }
+      if(get(i,i+mid)==get(k,k+mid)){
+          lo=mid;
+      }
+      else{
+          hi=mid;
+      }
+  }
+  if(s[i+lo]!=s[k+lo])lo--;
+  if(lo<n-1){
+      //cout<<s[i+lo+1]<<sp<<s[k+lo+1]<<endl;
+      if(s[i+lo+1]<s[k+lo+1]){//checking next element of common prefix
+          k=i;
+      }
+  }
+  //debug(k);
+ 
+}
+cout<<s.substr(k,n)<<endl;
+    return 0;
+
+}
+
+
+signed main()
+{
+    #ifndef KNIGHTMARE
+    freopen("Error.txt", "w", stderr);
+    #endif
+    
+    minato;
+    //w(t)
+    solve();
+
+
+    return 0;
+}
---
+++ String Problems/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+ //   if(i==8){
+
+    //       cout<<s.substr(i,mid+1)<<sp<<s.substr(k,mid+1)<<endl;
+    //       cout<<get(i,i+mid)<<sp<<get(k,k+mid)<<endl;
+          
+    //   }
