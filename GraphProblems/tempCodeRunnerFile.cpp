99e551acec6fc0d962ce6e628016ab3c226a1943
Mayank Raj <mayank42293@gmail.com>
2022-06-17 20:42:10 +0530

    17/6/22

---
+++ GraphProblems/tempCodeRunnerFile.cpp
@@ -0,0 +1,250 @@
+//                           ,;;;;;;,
+//                                 ,;;;'""`;;\
+//                               ,;;;/  .'`',;\
+//                             ,;;;;/   |    \|_
+//                            /;;;;;    \    / .\
+//                          ,;;;;;;|     '.  \/_/
+//                         /;;;;;;;|       \
+//              _,.---._  /;;;;;;;;|        ;   _.---.,_
+//            .;;/      `.;;;;;;;;;|         ;'      \;;,
+//          .;;;/         `;;;;;;;;;.._    .'         \;;;.
+//         /;;;;|          _;-"`       `"-;_          |;;;;\
+//        |;;;;;|.---.   .'  __.-"```"-.__  '.   .---.|;;;;;|
+//        |;;;;;|     `\/  .'/__\     /__\'.  \/`     |;;;;;|
+//        |;;;;;|       |_/ //  \   //  \ \_|       |;;;;;|
+//        |;;;;;|       |/ |/    || ||    \| \|       |;;;;;|
+//         \;;;;|    __ || _  .-.\| |/.-.  _ || __    |;;;;/
+//          \jgs|   / _\|/ = /_o_\   /_o_\ = \|/_ \   |;;;/
+//           \;;/   |`.-     `   `   `   `     -.`|   \;;/
+//          _|;'    \ |    _     _   _     _    | /    ';|_
+//         / .\      \_  ( '--'(     )'--' )  _//      /. \
+//         \/_/       \_/|  /_   |   |   _\  |\_/       \_\/
+//                       | /|\  \   /  //|\ |
+//                       |  | \'._'-'_.'/ |  |
+//                       |  ;  '-.```.-'  ;  |
+//                       |   \    ```    /   |
+//     __                ;    '.-"""""-.'    ;                __
+//    /\ \_         __..--\     `-----'     /--..__         _/ /\
+//    \_'/\`''---''`..;;;;.'.__,       ,__.',;;;;..`''---''`/\'_/
+//         '-.__'';;;;;;;;;;;,,'._   _.',,;;;;;;;;;;;''__.-'
+//              ``''--; ;;;;;;;;..`"`..;;;;;;;; ;--''``   _
+//         .-.       /,;;;;;;;';;;;;;;;;';;;;;;;,\    _.-' `\
+//       .'  /_     /,;;;;;;'/| ;;;;;;; |\';;;;;;,\  `\     '-'|
+//      /      )   /,;;;;;',' | ;;;;;;; | ',';;;;;,\   \   .'-./
+//      `'-..-'   /,;;;;','   | ;;;;;;; |   ',';;;;,\   `"`
+//               | ;;;','     | ;;;;;;; |  ,  ', ;;;'|
+//              _\__.-'  .-.  ; ;;;;;;; ;  |'-. '-.__/_
+//             / .\     (   )  \';;;;;'/   |   |    /. \
+//             \/_/   (`     `) \';;;'/    '-._|    \_\/
+//                     '-/ \-'   '._.'         `
+//                       """      /.`\
+//                                \|_/
+
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mk make_pair
+#define ll long long
+#define ss second
+#define ff first
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define mll map<ll,ll>
+//#define mod 1000000007
+#define sp " "
+#define w(x) ll x; cin>>x; while(x--)
+#define ps(x,y) fixed<<setprecision(y)<<x;
+#define fo(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define re(i, j) fo(i, 0, j, 1)
+#define pi 3.1415926535897932384626433832795
+#define all(cont) cont.begin(), cont.end()
+#define countbit(x) __builtin_popcount(x)
+#define mod 1000000007//998244353
+#define lo lower_bound
+#define de(n) ll n;cin>>n;
+#define def(a,n) ll n;cin>>n;ll a[n];re(i,n){cin>>a[i];}
+#define defi(a,n,k) ll n;cin>>n; ll k;cin>>k;ll a[n];re(i,n){cin>>a[i];}
+#define deb(x) cout<<#x<<"="<<x<<endl;
+#define tr(it,a) for(auto it=a.begin();it!=a.end();it++)
+#define nl cout<<endl;
+#define minato ios_base::sync_with_stdio(false), cin.tie(nullptr)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define precision(x) cout << fixed << setprecision(x);
+#define gcd(a,b)    __gcd((a),(b))
+#define lcm(a,b)    ((a)*(b)) / gcd((a),(b))
+//#define endl "\n"
+//#define int long long
+const int dx[4]={1,0,0,-1}, dy[4]={0,1,-1,0};
+const int x_dir[]={-1,-1,-1,0,0,1,1,1};
+const int y_dir[]={-1,0,1,-1,1,-1,0,1};
+
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+string s1="DRLU";
+#ifndef KNIGHTMARE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+//void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//KnightMareVoid
+vector<vector<int>> dp;
+int d[1001][1001];
+int vis[1001][1001];
+int solve(){
+    int n,m;
+    cin>>n>>m;
+    dp.assign(n,vector<int>(m,0));
+    queue<pair<int,pll>> q;
+    vector<string> v;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            dp[i][j]=INT_MAX;
+            d[i][j]=-1;
+        }
+    }
+    int x1,y1;
+    for(int i=0;i<n;i++){
+        string s;
+        cin>>s;
+        v.pb(s);
+        for(int j=0;j<m;j++){
+            if(s[j]=='M'){
+                q.push({0,{i,j}});
+                //dp[i][j]=0;
+            }
+            if(s[j]=='A'){
+                x1=i;
+                y1=j;
+            }
+
+        }
+    }
+    int x2=-1,y2=-1;
+    while(q.size()){
+        int c=q.front().first;
+        int x=q.front().second.first;
+        int y=q.front().second.second;
+      
+        q.pop();
+        dp[x][y]=c;
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx>=0 && nx<n && ny>=0 && ny<m && v[nx][ny]!='#'){
+                if(c+1<dp[nx][ny]){
+                    q.push({c+1,{nx,ny}});
+
+                
+                }
+
+            }
+        }
+
+
+    }
+    //for(int i=0;i<n;i++){
+    //     for(int j=0;j<m;j++){
+    //         cout<<dp[i][j]<<sp;
+    //     }
+    //     nl;
+    // }
+    
+    queue<pair<int,pll>> q1;
+    q1.push({0,{x1,y1}});
+    vis[x1][y1]=1;
+    while(q1.size()){
+        int x=q1.front().second.first;
+        int y=q1.front().second.second;
+        int c=q1.front().first;
+        q1.pop();
+        dp[x][y]=c;
+        //cout<<x<<sp<<y<<endl;
+        if(x==0 ||y==0||x==n-1||y==m-1){
+            x2=x;
+            y2=y;
+            break;
+        }
+        
+        for(int i=0;i<4;i++){
+            int nx=x+dx[i];
+            int ny=y+dy[i];
+            if(nx>=0 && nx<n && ny>=0 && ny<m && v[nx][ny]!='#'){
+                if(c+1<dp[nx][ny] ){
+                    d[nx][ny]=i;
+                    q1.push({c+1,{nx,ny}});
+                }
+
+            }
+        }
+
+    }
+    if(x2==-1 || y2==-1){
+        cout<<"NO"<<endl;
+        return 0;
+    }
+    string ans="";
+    while(1){
+        if(d[x2][y2]==-1)break;
+        ans+=s1[d[x2][y2]];
+        int px=x2;
+        int py=y2;
+        x2=px-dx[d[px][py]];
+        y2=py-dy[d[px][py]];
+
+    }
+    reverse(all(ans));
+    cout<<"YES"<<endl;
+    cout<<ans.size()<<endl;
+    cout<<ans<<endl;
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
+
+
+signed main()
+{
+    #ifndef KNIGHTMARE
+    freopen("Error.txt", "w", stderr);
+    #endif
+    
+    minato;
+    //w(t)
+    solve();
+
+
+    return 0;
+}
