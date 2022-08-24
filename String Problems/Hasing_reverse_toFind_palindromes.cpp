7120348ff39d93677349a11cf5c22729a261f661
Mayank9mare <mayank42293@gmail.com>
2022-08-24 14:37:48 +0530

    24/8/22

---
+++ String Problems/Hasing_reverse_toFind_palindromes.cpp
@@ -0,0 +1,194 @@
+//                           ,;;;;;;,
+//                                 ,;;;'""`;;\
+//                               ,;;;/  .'`',;\
+//                             ,;;;;/   |    \|_
+//                            /;;;;;    \    / .\
+//                          ,;;;;;;|     '.  \/_/
+//                         /;;;;;;;|       \
+//              _,.---._  /;;;;;;;;|        ;   _.---.,_
+//            .;;/      `.;;;;;;;;;|         ;'      \;;,
+//          .;;;/         `;;;;;;;;;.._    .'         \;;;.
+//         /;;;;|          _;-"`       `"-;_          |;;;;\
+//        |;;;;;|.---.   .'  __.-"```"-.__  '.   .---.|;;;;;|
+//        |;;;;;|     `\/  .'/__\     /__\'.  \/`     |;;;;;|
+//        |;;;;;|       |_/ //  \   //  \ \_|       |;;;;;|
+//        |;;;;;|       |/ |/    || ||    \| \|       |;;;;;|
+//         \;;;;|    __ || _  .-.\| |/.-.  _ || __    |;;;;/
+//          \jgs|   / _\|/ = /_o_\   /_o_\ = \|/_ \   |;;;/
+//           \;;/   |`.-     `   `   `   `     -.`|   \;;/
+//          _|;'    \ |    _     _   _     _    | /    ';|_
+//         / .\      \_  ( '--'(     )'--' )  _//      /. \
+//         \/_/       \_/|  /_   |   |   _\  |\_/       \_\/
+//                       | /|\  \   /  //|\ |
+//                       |  | \'._'-'_.'/ |  |
+//                       |  ;  '-.```.-'  ;  |
+//                       |   \    ```    /   |
+//     __                ;    '.-"""""-.'    ;                __
+//    /\ \_         __..--\     `-----'     /--..__         _/ /\
+//    \_'/\`''---''`..;;;;.'.__,       ,__.',;;;;..`''---''`/\'_/
+//         '-.__'';;;;;;;;;;;,,'._   _.',,;;;;;;;;;;;''__.-'
+//              ``''--; ;;;;;;;;..`"`..;;;;;;;; ;--''``   _
+//         .-.       /,;;;;;;;';;;;;;;;;';;;;;;;,\    _.-' `\
+//       .'  /_     /,;;;;;;'/| ;;;;;;; |\';;;;;;,\  `\     '-'|
+//      /      )   /,;;;;;',' | ;;;;;;; | ',';;;;;,\   \   .'-./
+//      `'-..-'   /,;;;;','   | ;;;;;;; |   ',';;;;,\   `"`
+//               | ;;;','     | ;;;;;;; |  ,  ', ;;;'|
+//              _\__.-'  .-.  ; ;;;;;;; ;  |'-. '-.__/_
+//             / .\     (   )  \';;;;;'/   |   |    /. \
+//             \/_/   (`     `) \';;;'/    '-._|    \_\/
+//                     '-/ \-'   '._.'         `
+//                       """      /.`\
+//                                \|_/
+
+
+#include<bits/stdc++.h>
+#define pb push_back
+#define mk make_pair
+#define ll long long
+#define ss second
+#define ff first
+#define pll pair<ll,ll>
+#define vll vector<ll>
+#define mll map<ll,ll>
+//#define mod 1000000007
+#define sp " "
+#define w(x) ll x; cin>>x; while(x--)
+#define ps(x,y) fixed<<setprecision(y)<<x;
+#define fo(i, j, k, in) for (ll i=j ; i<k ; i+=in)
+#define re(i, j) fo(i, 0, j, 1)
+#define pi 3.1415926535897932384626433832795
+#define all(cont) cont.begin(), cont.end()
+#define countbit(x) __builtin_popcount(x)
+#define mod 1000000007//998244353
+#define lo lower_bound
+#define de(n) ll n;cin>>n;
+#define def(a,n) ll n;cin>>n;ll a[n];re(i,n){cin>>a[i];}
+#define defi(a,n,k) ll n;cin>>n; ll k;cin>>k;ll a[n];re(i,n){cin>>a[i];}
+#define deb(x) cout<<#x<<"="<<x<<endl;
+#define tr(it,a) for(auto it=a.begin();it!=a.end();it++)
+#define nl cout<<endl;
+#define minato ios_base::sync_with_stdio(false), cin.tie(nullptr)
+#define mem1(a)           memset(a,-1,sizeof(a))
+#define mem0(a)           memset(a,0,sizeof(a))
+#define ppc               __builtin_popcount
+#define ppcll             __builtin_popcountll
+#define precision(x) cout << fixed << setprecision(x);
+#define gcd(a,b)    __gcd((a),(b))
+#define lcm(a,b)    ((a)*(b)) / gcd((a),(b))
+#define endl "\n"
+#define int long long
+const int dx[4]={1,0,0,-1}, dy[4]={0,1,-1,0};
+const int x_dir[]={-1,-1,-1,0,0,1,1,1};
+const int y_dir[]={-1,0,1,-1,1,-1,0,1};
+
+
+using namespace std;
+typedef unsigned long long ull;
+typedef long double lld;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+
+#ifndef KNIGHTMARE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+//void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+//KnightMareVoid
+vector<int> hashed(string s,vector<int>&pow){
+    int n=s.size();
+    pow.assign(n,0);
+pow[0]=1;
+int p=31;
+for(int i=1;i<n;i++){
+    pow[i]=(pow[i-1]*p)%mod;
+}
+vector<int> h(n+1,0);
+for(int i=0;i<n;i++){
+    h[i+1]=(h[i]+(s[i]-'a'+1)*pow[i])%mod;
+}
+return h;
+
+}
+int isPal(string s){
+    int n=s.size();
+    for(int i=0;i<n;i++){
+        if(s[i]!=s[n-i-1])return 0;
+    }
+    return 1;
+}
+int chk(int i,int j,int n,vector<int> &h1,vector<int> &h2,vector<int> &pow1,vector<int> &pow2){
+
+     int lr=(h1[j+1]-h1[i]+mod)%mod;
+     lr=(lr*pow2[n-j-1])%mod;
+    int rr=(h2[n-i]-h2[n-j-1]+mod)%mod;
+    rr=(rr*pow1[i])%mod;
+    if(lr==rr)return 1;
+    return 0;
+
+}
+int solve(int t){
+    cout<<"Case"<<sp<<"#"<<t<<":"<<sp;
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+   
+    string k="";
+  
+    string t1=s;
+    reverse(all(t1));
+    vector<int> pow1;
+    vector<int> pow2;
+    vector<int> h1=hashed(s,pow1);
+    vector<int> h2=hashed(t1,pow2);
+    for(int i=0;i<n;i++){
+        k=k+s[i];
+        if(chk(0,i,n,h1,h2,pow1,pow2) && chk(i+1,n-1,n,h1,h2,pow1,pow2)){
+            cout<<k<<endl;
+            return 0;
+        }
+         
+
+
+    }
+
+
+    return 0;
+
+}
+
+
+signed main()
+{
+    #ifndef KNIGHTMARE
+    freopen("Error.txt", "w", stderr);
+    #endif
+    
+    minato;
+    int c=0;
+    w(t){
+        c++;
+        solve(c);
+    }
+
+
+    return 0;
+}
